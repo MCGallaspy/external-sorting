1f48f15a52c6743702d1497308d5669151e65f9a
Michael Gallaspy <gallaspy.michael@gmail.com>
2017-01-24 22:47:19 -0800

    Create file-opening example

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 main.exe: main.cc
-	c++ `pkg-config --cflags --libs $(SEASTAR)/build/release/seastar.pc` main.cc -o main.exe
+	g++-5 -Wall -Werror -std=c++14 $(EXTRA_FLAGS) `pkg-config --cflags --libs $(SEASTAR)/build/release/seastar.pc` main.cc -o main.exe
--- main.cc
+++ main.cc
@@ -1,8 +1,19 @@
+#include "core/aligned_buffer.hh"
 #include "core/app-template.hh"
+#include "core/file.hh"
 #include "core/reactor.hh"
+#include "core/semaphore.hh"
 #include "core/sstring.hh"
 
 #include <iostream>
+
+static constexpr size_t PAGE_SIZE = 4096; // I checked it for my system
+static constexpr uint64_t BYTES_PER_RUN = PAGE_SIZE*10ull; // arbitrary value, adjust after benchmarking
+
+struct file_wrapper {
+    file_wrapper(file&& f) : f(std::move(f)) {}
+    file f;
+};
 
 int main(int argc, char** argv) {
     if (argc < 2) {
@@ -12,11 +23,36 @@ int main(int argc, char** argv) {
     sstring filename(argv[1]);
     int fake_argc = 1;
     
+    semaphore done {0};
+
     app_template app;
-    app.run(fake_argc, argv, [] {
-            std::cout << smp::count << "\n";
-            return make_ready_future<>();
+    app.run(fake_argc, argv,
+    [filename, &done] {
+        open_file_dma(filename, open_flags::rw).then(
+        [&done] (file f) {
+            auto fw = new file_wrapper{std::move(f)};
+            uint64_t file_size = fw->f.size().get0();
+            const uint32_t rem = file_size % BYTES_PER_RUN == 0 ? 0 : 1;
+            const uint32_t num_runs = rem + file_size / BYTES_PER_RUN;
+            std::cout << "filesize is " << file_size << "\n"
+                      << "num runs is " << num_runs << "\n";
+            for (uint32_t i = 0; i < num_runs; ++i) {
+                uint64_t remaining = file_size - (i * BYTES_PER_RUN);
+                const size_t bufsize = std::min<size_t>(remaining, BYTES_PER_RUN);
+                std::cout << "bufsize is " << bufsize << "\n";
+                auto buf = allocate_aligned_buffer<unsigned char>(bufsize, PAGE_SIZE);  
+                fw->f.dma_read(i * PAGE_SIZE, buf.get(), bufsize).then(
+                [buf = std::move(buf), &done] (size_t bytes_read) mutable {
+                    std::cout << "Read " << bytes_read << " bytes\n";
+                    std::cout.write(reinterpret_cast<char*>(buf.get()), bytes_read);
+                    std::cout.flush();
+                    done.signal();
+                });
+            }
+        });
+        return make_ready_future();
     });
     
+    done.wait();
     return 0;
 }
